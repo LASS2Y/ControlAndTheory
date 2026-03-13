6ab8b17d17dba6a18919be6eb460dbad557b4da2
charlesnoah344 <23397@ecam.be>
2026-03-13 14:35:29 +0100

    IMCTuning implémenté

--- package_lab.py
+++ package_lab.py
@@ -41,3 +41,39 @@ def LL_RT(MV,Kp,Tlag,Tlead,Ts,PV,PVInit=0,method='EBD'):
                 PV.append((1/(1+K))*PV[-1] + (K*Kp/(1+K)) * ((1+(Tlead/(Ts)))*MV[-1] - (Tlead/(Ts))*MV[-2]))
     else:
         PV.append(Kp*MV[-1])
+
+def IMCTuning(Kp, Tlag1, Tlag2, theta, gamma, process="SOPDT"):
+        """
+        IMCTuning computes the IMC PID tuning parameters for FOPDT and SOPDT processes.
+        Kp: process gain (Kp)
+        Tlag1: first (main) lag time constant [s]
+        Tlag2: second lag time constant [s]
+        theta: delay [s]
+        gamma: used to computed the desired closed loop time constant Tclp [s] (range [0.2 -> 0.9])
+        process:
+            FOPDT-PI: First Order Plus Dead Time for P-I control (IMC tuning case G)
+            FOPDT-PID: First Order Plus Dead Time for P-I-D control (IMC tuning case H)
+            SOPDT :Second Order Plus Dead Time for P-I-D control (IMC tuning case I)
+            
+        return : PID controller parameters Kc, Ti and Td
+        """
+        
+
+        Tclp = gamma*Tlag1 
+        if process=="FOPDT-PI":
+            Kc = (Tlag1/(Tclp+theta))/Kp
+            Ti = Tlag1
+            Td = 0
+        elif process=="FOPDT-PID":
+            Kc= ((Tlag1 + theta/2)/(Tclp + theta/2))/Kp
+            Ti = Tlag1 + theta/2
+            Td = (Tlag1*theta)/(2*Tlag1+theta)
+        elif process=="SOPDT": 
+            Kc = ((Tlag1 + Tlag2)/(Tclp + theta))/Kp
+            Ti = (Tlag1 +Tlag2)
+            Td = ((Tlag1*Tlag2))/(Tlag1+Tlag2)
+        else:
+            Kc = (Tlag1/(Tclp+theta))/Kp
+            Ti = Tlag1
+            Td = 0
+        return (Kc, Ti, Td)
